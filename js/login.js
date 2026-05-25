3658fccb0ce73c44c5b05fe40c34c05012c5afe3
Ingeniero Luis Grisales <contactoluiancol@gmail.com>
2026-05-25 16:19:40 -0500

    Update Supabase connection and login logic

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ const supabaseUrl =
 'https://hurxdjoiafkjoyrmyhbd.supabase.co';
 
 const supabaseKey =
-'TU_API_KEY';
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1cnhkam9pYWZram95cm15aGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MzgxMTMsImV4cCI6MjA5NTMxNDExM30.Z6fRiWft3eSEVNZbWflmcvVcHAJTAEA37tPdp4LRnTg';
 
 const supabase = window.supabase.createClient(
 
